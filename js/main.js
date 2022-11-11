c908b79ef2fba401359a6299e805b5baeae3c5d8
mariangellinda <117944192+mariangellinda@users.noreply.github.com>
2022-11-11 16:00:34 -0500

    commit inicial
    commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+
+function Circulo(){
+    var figura = document.getElementById('figura');
+    figura.classList.toggle('Circulo');
+}
+
+function Rombo(){
+    var figura = document.getElementById('figura');
+    figura.classList.toggle('Rombo');
+}
+
+function Gif(){
+    var figura = document.getElementById('figura');
+    figura.classList.toggle('Gif');
+}
+
+function Triangulo() {
+    var figura= document.getElementById('figura');
+    figura.classList.toggle("Triangulo")}
+
+
+
+function arriba(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("top");
+}
+
+function izquierda(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("Left");
+}
+
+function abajo(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("Butom");
+}
+
+function derecha(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("Right");
+}
+
+function DiagonalIz(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("DiagonalIz");
+}
+function DiagonalDe(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("DiagonalDe");
+}
+
+function gif(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("gif"); 
+}
